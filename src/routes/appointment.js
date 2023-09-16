6a43f9f40ae6da621ddf5f00429d5d86aea89634
BenKungu <benwaweru2013@gmail.com>
2023-09-16 08:34:10 +0300

    routes for appointment data

--- src/routes/appointment.js
+++ src/routes/appointment.js
@@ -1,10 +1,33 @@
+// routes/appointments.js
+
 const express = require("express");
 const router = express.Router();
+const { Appointment } = require("../models");
+const sequelize = require("../config/database");
 
-// Define your API routes here
-router.get("/", (req, res) => {
-  // Handle GET request for appointments
-  res.json({ message: "Get appointments" });
+router.post("/appointments", async (req, res) => {
+  try {
+    // Get form data from the request
+    const { name, email, phone, meeting, location, service } = req.body;
+
+    // Create a new appointment record in the database
+    const appointment = await Appointment.create({
+      name,
+      email,
+      phone,
+      meeting,
+      location,
+      service,
+    });
+
+    // Respond with a success message
+    res
+      .status(201)
+      .json({ message: "Appointment created successfully", appointment });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 });
 
 module.exports = router;
