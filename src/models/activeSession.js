9acab40023377737124abebf7bf7b4066a9b0ee1
BenKungu <benwaweru2013@gmail.com>
2023-09-23 21:27:45 +0300

    auth sessions

--- src/models/activeSession.js
+++ src/models/activeSession.js
@@ -9,3 +9,14 @@ const activeSessionSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  const mongoose = require('mongoose');
+
+const activeSessionSchema = new mongoose.Schema({
+  token: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
