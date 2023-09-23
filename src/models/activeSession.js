fc3fcd0a57ee454a998faf7d9243b5ebf5e697f0
BenKungu <benwaweru2013@gmail.com>
2023-09-23 21:27:20 +0300

    session

---
+++ src/models/activeSession.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const activeSessionSchema = new mongoose.Schema({
+  token: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
